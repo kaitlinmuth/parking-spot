8e79944233aa846153e205d746836db1a4e6837d
kaitlinmuth <kaitlin.muth@gmail.com>
2015-05-26 13:59:13 -0500

    Initial commit; implementing MEAN stack skeleton

---
+++ Gruntfile.js
@@ -0,0 +1,42 @@
+module.exports = function(grunt) {
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                src: 'client/app.js',
+                dest: 'server/public/assets/scripts/app.min.js'
+            }
+        },
+        copy: {
+            angular: {
+                expand: true,
+                cwd: "node_modules/",
+                src: [
+                    "angular/angular.min.js",
+                    "angular/angular.min.js.map",
+                    "angular/angular-csp.css"
+                ],
+                dest: "server/public/vendor/"
+            },
+            css: {
+                expand: true,
+                cwd: "client/",
+                src: [
+                    "style.css"
+                ],
+                dest: "server/public/assets/css/"
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    // Default task(s).
+    grunt.registerTask('default', ['copy', 'uglify']);
+
+};
